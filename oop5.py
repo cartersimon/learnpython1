748b3881a4bf34fad6dc24560bf5a5c325e9f486
cartersimon <carter.simon@gmail.com>
2022-01-29 08:48:31 +0100

    final bit of oop beginner youtube video

--- oop5.py
+++ oop5.py
@@ -1,8 +1,9 @@
+# Put static methods into a class to keep them together and so that they can easily be reused
+
 class Math:
     
     @staticmethod
-    def add5(x):      # no need for "self" as not acting on an object, just running the function
+    def add5(x):      # no need for "self" or "cls" as not acting on an object (they don't have access), just runs as a function
         return x + 5 
-
 
 print(Math.add5(4))
