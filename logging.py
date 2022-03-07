372f6e43844729f0210596f65eb5f99aaa765375
cartersimon <carter.simon@gmail.com>
2022-03-07 22:51:13 +0100

    Intmed python YT video pt11- logging (revisit topic)

--- logging.py
+++ logging.py
@@ -2,6 +2,21 @@
 # Intermediate Python Programming Course (freecodecamp.org)
 
 # Logging
+import logging                                  # 5 diff log levels
 
+# to adjust default display behaviour of log msgs...
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)s')
+# above doesn't seem to be working for me, check docs/other tutorials
 
-# 2:20:11
+#logging.debug('debug msg')                      # not printed by default
+#logging.info('info msg')                        # not printed by default
+#logging.warning('warning msg')                  # printed
+#logging.error('error msg')                      # printed
+#logging.critical('critical msg')                # printed
+
+# by default the logger is called the "root" logger
+# to log in other modules, best practice is not to use "root"
+# create your own module logger
+import logging_helper
+
+# 2:26:07
